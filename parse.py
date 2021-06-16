42d6fc54f9c1715b6b747e2c3c5ab731d91260e0
workdl <workdl@local.loc>
2021-06-16 12:45:11 +0300

    add bot + parse
    'https://freegames.codes'

---
+++ parse.py
@@ -0,0 +1,76 @@
+import json
+import time
+
+import requests
+from bs4 import BeautifulSoup
+
+DOMAIN = 'https://freegames.codes'
+
+
+def print_target(link):
+    print(link)
+    r = requests.get(link)
+    soup = BeautifulSoup(r.text, 'lxml')
+    details_title = soup.find('div', class_='details__wrap').find('h1').text.strip()
+    details_list = soup.find('div', class_='details__wrap').find('ul').text.strip()
+    details_image = soup.find('div', class_='details__cover').find('img').get('src').strip()
+    details_buy = soup.find('a', class_='details__buy').get('href').strip()
+
+    return details_image, details_title, details_list, details_buy
+
+
+def check_target(links):
+    targets_querry = {}
+
+    with open('old_target.json', 'r') as f:  # загружаем старые таргеты
+        targets_old = json.load(f)
+
+    with open('old_target.json', 'w') as f:
+
+        for i in links:
+            target = i.split('/')[4]
+            if target in targets_old:
+                print('Таргет уже в базе:', i)
+            else:
+                details_image, details_title, details_list, details_buy = print_target(i)
+                print('*** Таргет на публикацию ***', '\n' + i)
+                print('- details_image:', details_image)
+                print('- details_title:', details_title)
+                print('- details_list:', details_list)
+                print('- details_buy:', details_buy)
+                print('*** конец информаци о таргете ***')
+
+                targets_querry[target] = {
+                        'details_image': details_image,
+                        'details_title': details_title,
+                        'details_list': details_list,
+                        'details_buy': details_buy,
+                        'push_groupe_id': []
+                    }
+                time.sleep(15)
+        targets_all = {**targets_old, **targets_querry}
+        json.dump(targets_all, f, indent=4, ensure_ascii=False)
+
+    return targets_querry
+
+
+def start_parse_all_games():
+    r = requests.get('https://freegames.codes/game/')
+
+    links = set()
+    target_id = set()
+    soup = BeautifulSoup(r.text, "lxml")
+
+    for pos in soup.findAll('a', class_='card__cover'):
+        target = pos.get('href')
+        links.add('https://freegames.codes/game/' + target)
+        target_id.add(target.split('/')[0])
+
+    print('id на странице all_free_games:', target_id)
+    print(links)
+
+    return check_target(links)
+
+
+if __name__ == "__main__":
+    start_parse_all_games()
---
+++ tg_bot.py
@@ -0,0 +1,71 @@
+import asyncio
+import json
+
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+from config import TOKEN
+from parse import start_parse_all_games, DOMAIN
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+markdown = """
+    *bold text*
+    """
+
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    id_chat = open('id_chat.txt').read().split()
+
+    if str(message.chat.id) in id_chat:
+        start_id_chat = open('start_id_chat.txt').read().split()
+        start_id_chat.append(str(message.chat.id))
+
+        with open('start_id_chat.txt', 'w') as f:
+            for id in set(start_id_chat):
+                f.write(id + '\n')
+        await bot.send_message(message.chat.id, 'Статус бота: Запущен')
+    else:
+        await bot.send_message(message.chat.id, 'Ваша группа не зарегистрирована')
+        print('Попытка запустить бота в не зарегистрированной группе:', message.chat.id)
+
+
+
+
+
+async def push_sell():
+    while True:
+        # start_parse_all_games()
+        start_id_chat = open('start_id_chat.txt').read().split()
+        with open('old_target.json', 'r') as f:  # загружаем таргеты
+            list_sell = json.load(f)
+
+        for id_chat in start_id_chat: # проверка и выгрузка таргета распродажи в группы.
+            for target in list_sell:
+
+                push_groupe_id = list_sell[target]['push_groupe_id']
+
+                if id_chat in push_groupe_id:
+                    # print('Скидка в этой группе опубликована')
+                    continue
+
+                else:
+                    add_groupe_id = [id_chat]
+                    list_sell[target]['push_groupe_id'] = push_groupe_id + add_groupe_id
+                    await bot.send_photo(id_chat, DOMAIN + list_sell[target]['details_image'],
+                                                 list_sell[target]['details_list'] + '\n' +
+                                                 list_sell[target]['details_buy'])
+        with open('old_target.json', 'w') as f:
+            json.dump(list_sell, f, indent=4, ensure_ascii=False)
+
+        await asyncio.sleep(60)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.create_task(push_sell())
+    executor.start_polling(dp)
+
