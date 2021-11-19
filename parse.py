e6efcfc5536be32d098d0a4d909436d22d63af8c
danil <danil.spb.it@gmail.com>
2021-11-19 18:54:39 +0300

    fix

--- parse.py
+++ parse.py
@@ -8,7 +8,7 @@ DOMAIN = 'https://freegames.codes'
 
 
 def print_target(link):
-    print(link)
+    # print(link)
     r = requests.get(link)
     soup = BeautifulSoup(r.text, 'lxml')
     details_title = soup.find('div', class_='details__wrap').find('h1').text.strip()
@@ -31,17 +31,17 @@ def check_target(links):
         for i in links:
             target = i.split('/')[4]
             if target in targets_old:
-                print('Таргет уже в базе:', i)
+                # print('Таргет уже в базе:', i)
                 adds_target.append(target)
 
             else:
                 details_image, details_title, details_list, details_buy = print_target(i)
-                print('*** Таргет на публикацию ***', '\n' + i)
-                print('- details_image:', details_image)
-                print('- details_title:', details_title)
-                print('- details_list:', details_list)
-                print('- details_buy:', details_buy)
-                print('*** конец информаци о таргете ***')
+                # print('*** Таргет на публикацию ***', '\n' + i)
+                # print('- details_image:', details_image)
+                # print('- details_title:', details_title)
+                # print('- details_list:', details_list)
+                # print('- details_buy:', details_buy)
+                # print('*** конец информаци о таргете ***')
 
                 targets_querry[target] = {
                         'details_image': details_image,
@@ -80,8 +80,8 @@ def start_parse_all_games():
         links.add('https://freegames.codes/game/' + target)
         target_id.add(target.split('/')[0])
 
-    print('id на странице all_free_games:', target_id)
-    print(links)
+    # print('id на странице all_free_games:', target_id)
+    # print(links)
 
     return check_target(links)
 
